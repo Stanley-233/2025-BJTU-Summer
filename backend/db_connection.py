2a2b1057ad07960b973beec1c1766728ac82cc04
Ashens123 <1920385205@qq.com>
2025-07-16 20:15:26 +0800

    feat:动态可视化的仪表盘，更新过的数据可视化和分类

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -8,12 +8,11 @@ class DatabaseConnection:
 
     def get_speed_data(self, start_date=None, end_date=None):
         if start_date and end_date:
-            query = f"SELECT timestamp, average_speed FROM speed_data WHERE timestamp BETWEEN '{start_date}' AND '{end_date}'"
+            query = f"SELECT time_interval_start, average_speed FROM five_min_speed WHERE time_interval_start BETWEEN '{start_date}' AND '{end_date}'"
         else:
-            query = "SELECT timestamp, average_speed FROM speed_data"
+            query = "SELECT time_interval_start, average_speed FROM five_min_speed"
         df = pd.read_sql(query, self.engine)
-        # 将速度从 cm/s 转换为 km/h
-        df['average_speed'] = df['average_speed'] * 0.036
+        df['average_speed'] = df['average_speed']
         return df.to_dict(orient='records')
 
     def get_od_data(self, start_date=None, end_date=None):
@@ -29,5 +28,13 @@ class DatabaseConnection:
             query = f"SELECT hour, active_taxis, temperature, humidity, wind_speed FROM hourly_taxi_weather WHERE hour BETWEEN '{start_date}' AND '{end_date}'"
         else:
             query = "SELECT hour, active_taxis, temperature, humidity, wind_speed FROM hourly_taxi_weather"
+        df = pd.read_sql(query, self.engine)
+        return df.to_dict(orient='records')
+
+    def get_trip_percentage_data(self, start_date=None, end_date=None):
+        if start_date and end_date:
+            query = f"SELECT date, medium_trip, short_trip, long_trip FROM trip_percentage WHERE date BETWEEN '{start_date}' AND '{end_date}'"
+        else:
+            query = "SELECT date, medium_trip, short_trip, long_trip FROM trip_percentage"
         df = pd.read_sql(query, self.engine)
         return df.to_dict(orient='records')
